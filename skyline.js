c5ebb519fe4309dfbc800d5ff7acca729b62739a
Andrew Davis <andrew@moodle.com>
2013-07-18 20:46:20 +0800

    fixed spelling

--- skyline.js
+++ skyline.js
@@ -1,5 +1,5 @@
 /*
- * A class that draws a simple styalized side scrolling city scene.
+ * A class that draws a simple stylized side scrolling city scene.
  *
  * This code does not come with any sort of warranty.
  * You are welcome to use it for whatever you like.
